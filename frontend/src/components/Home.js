0edcbfedbbdcfa94b7ca429993257c06baa317dd
aidanflinton <69019017+aidanflinton@users.noreply.github.com>
2022-06-06 17:17:16 -0400

    home item grid links to item page, need to pass the params from the button to the link

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,6 +2,9 @@ import React from "react";
 import Navbar from "./Navbar";
 import { useState, useEffect, useContext } from "react";
 
+import { Link, Outlet, useNavigate } from "react-router-dom";
+
+import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -26,13 +29,17 @@ function Home(props) {
       >
         {props.items.map((item) => (
           <Grid item xs={12} sm={6} md={3} key={props.items.indexOf(item)}>
-            <Card sx={{ minWidth: 275, maxWidth: 345 }}>
-              <CardMedia />
-              <CardHeader title={item.title} />
-              <CardContent>
-                <Typography variant="body">{item.description}</Typography>
-              </CardContent>
-            </Card>
+            <Button>
+              <Link to="/item">
+                <Card sx={{ minWidth: 275, maxWidth: 345 }}>
+                  <CardMedia />
+                  <CardHeader title={item.title} />
+                  <CardContent>
+                    <Typography variant="body">{item.description}</Typography>
+                  </CardContent>
+                </Card>
+              </Link>
+            </Button>
           </Grid>
         ))}
       </Grid>
