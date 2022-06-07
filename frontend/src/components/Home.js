1370df38c821d151898798a7758f5da9d64289ea
YC Pan <yp9ar@virginia.edu>
2022-06-07 17:20:00 -0400

    landing page styling updated

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,45 +1,64 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { useState, useEffect, useContext } from "react";
-
-import { Link, Outlet, useNavigate } from "react-router-dom";
-
-import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import CardHeader from "@mui/material/CardHeader";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import { Carousel } from "react-bootstrap";
-import Avatar from "@mui/material/Avatar";
-import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import { Carousel } from 'react-bootstrap';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import PaginationBasic from './PaginationBasic'
 
-function Home(props) {
-  const [bookList, setBookList] = useState(null);
+// import PaginationBasic from './PaginationBasic'
+import "../App.css";
+
+function Home() {
+  const [books,setBooks] = useState([]);
+  const [loading,setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [bookPerPage, setBookPerPage] = useState(8);
+
+
+  const indexOfLastBook = currentPage * bookPerPage;
+  const indexOfFirstBook = indexOfLastBook - bookPerPage;
+  const currentBooks = books.slice(indexOfFirstBook,indexOfLastBook)
 
   useEffect(() => {
     fetch("http://localhost:9000/store/subjects?subject=" + "love")
       .then((res) => res.json())
       //.then((data) => console.log(data))
-      .then((data) => setBookList(data))
-      .then(console.log(bookList));
+      .then((data) => setBooks(data.works))
+ 
     fetch(
       "http://localhost:9000/store/author?authorkey=" + "/authors/OL4327048A"
     )
       .then((res) => res.json())
       .then((data) => console.log(data));
   }, []);
+
+  const paginate=(pageNumber)=>setCurrentPage(pageNumber);
+
+
   return (
     <>
-      <div className="home--container">
-        <div className="carousel--container">
-          <Carousel className="carouselImage">
-            <Carousel.Item>
-              <img
-                className="d-block w-100 "
-                src="https://cdn.domestika.org/c_fill,dpr_auto,f_auto,pg_1,t_base_params/v1633017596/course-covers/000/002/493/2493-original.jpg?1633017596"
-                alt="First slide"
+      {/* <Navbar setPage={"Home"}></Navbar> */}
+      <br></br>
+      <br></br>
+      <br></br>
+    <div className="home--container">
+      
+      <div className="carousel--container">
+        <Carousel className="carouselImage">
+          <Carousel.Item>
+            <img
+              className="d-block w-100 "
+              src="https://cdn.domestika.org/c_fill,dpr_auto,f_auto,pg_1,t_base_params/v1633017596/course-covers/000/002/493/2493-original.jpg?1633017596"
+              alt="First slide"
               />
               <Carousel.Caption>
                 <h3>First slide label</h3>
@@ -78,74 +97,50 @@ function Home(props) {
           </Carousel>
         </div>
         <Stack direction="row" spacing={2} className="avatar--container">
-          <div>
-            <Avatar
-              alt="Remy Sharp"
-              src="https://i.etsystatic.com/26564732/r/il/94c82b/2978999220/il_300x300.2978999220_m8mh.jpg"
-              sx={{ width: 100, height: 100 }}
-            />
-            <p>Under $10</p>
-          </div>
-          <div>
-            <Avatar
-              alt="Remy Sharp"
-              src="https://i.etsystatic.com/26564732/r/il/94c82b/2978999220/il_300x300.2978999220_m8mh.jpg"
-              sx={{ width: 100, height: 100 }}
-            />
-            <p>Under $10</p>
-          </div>
-          <div>
-            <Avatar
-              alt="Remy Sharp"
-              src="https://i.etsystatic.com/26564732/r/il/94c82b/2978999220/il_300x300.2978999220_m8mh.jpg"
-              sx={{ width: 100, height: 100 }}
-            />
-            <p>Under $10</p>
-          </div>
-          <div>
-            <Avatar
-              alt="Remy Sharp"
-              src="https://i.etsystatic.com/26564732/r/il/94c82b/2978999220/il_300x300.2978999220_m8mh.jpg"
-              sx={{ width: 100, height: 100 }}
-            />
-            <p>Under $10</p>
-          </div>
-          <div>
-            <Avatar
-              alt="Remy Sharp"
-              src="https://i.etsystatic.com/26564732/r/il/94c82b/2978999220/il_300x300.2978999220_m8mh.jpg"
-              sx={{ width: 100, height: 100 }}
-            />
-            <p>Under $10</p>
-          </div>
-        </Stack>
+        <div>
+        <Avatar alt="Remy Sharp" src="https://i.etsystatic.com/26564732/r/il/94c82b/2978999220/il_300x300.2978999220_m8mh.jpg" sx={{ width: 100, height: 100 }}/>
+        <p>Fiction</p>
+        </div>
+        <div>
+        <Avatar alt="Remy Sharp" src="https://i.etsystatic.com/26564732/r/il/94c82b/2978999220/il_300x300.2978999220_m8mh.jpg" sx={{ width: 100, height: 100 }}/>
+        <p>Romance</p>
+        </div>
+        <div>
+        <Avatar alt="Remy Sharp" src="https://i.etsystatic.com/26564732/r/il/94c82b/2978999220/il_300x300.2978999220_m8mh.jpg" sx={{ width: 100, height: 100 }}/>
+        <p>Drama</p>
+        </div>
+        <div>
+        <Avatar alt="Remy Sharp" src="https://i.etsystatic.com/26564732/r/il/94c82b/2978999220/il_300x300.2978999220_m8mh.jpg" sx={{ width: 100, height: 100 }}/>
+        <p>Classic Literature</p>
+        </div>
+        <div>
+        <Avatar alt="Remy Sharp" src="https://i.etsystatic.com/26564732/r/il/94c82b/2978999220/il_300x300.2978999220_m8mh.jpg" sx={{ width: 100, height: 100 }}/>
+        <p>Adventure</p>
+        </div>
+       
+     </Stack>
 
-        <br></br>
-        <br></br>
 
-        <Grid
-          container
-          spacing={2}
-          direction="row"
-          alignItems="center"
-          justify="center"
-          className="grid--container"
-        >
-          {bookList &&
-            bookList.works.map((work) => (
-              <Grid item xs={12} sm={3} key={bookList.works.indexOf(work)}>
-                <Button>
-                  <Link
-                    to="/item"
-                    state={{
+      
+      <div className="">
+      <Grid
+                container
+                spacing={2}
+                direction="row"
+                alignItems="center"
+                justify="center"
+                className="grid--container"
+            >
+                {currentBooks.map((work) => (
+                <Grid item xs={12} sm={6} md={3} key={currentBooks.indexOf(work)}>
+                    <Button>
+                    <Link to="/item" state={{
                       title: work.title,
                       authors: work.authors[0].name,
                       coverId: work.cover_id,
-                      bookId: work.key,
-                    }}
-                  >
-                    <Card sx={{ minWidth: 275, maxWidth: 345 }}>
-                      <CardMedia
+                    }}>
+                        <Card sx={{ minWidth: 275, maxWidth: 345 }} className="card">
+                        <CardMedia
                         component="img"
                         height="194"
                         image={
@@ -155,19 +150,20 @@ function Home(props) {
                         }
                         alt="Title"
                       />
-                      <CardHeader title={work.title} />
-                      <CardContent>
-                        <Typography variant="body">
-                          {work.authors[0].name}
-                        </Typography>
-                      </CardContent>
-                    </Card>
-                  </Link>
-                </Button>
-              </Grid>
-            ))}
-          {!bookList && <div>Loading...</div>}
-        </Grid>
+                        <CardHeader title={work.title} />
+                        <CardContent className="card--content">
+                            <Typography variant="body">{work.authors[0].name}</Typography>
+                        </CardContent>
+                        </Card>
+                    </Link>
+                    </Button>
+                </Grid>
+                ))}
+               
+            </Grid>
+        <PaginationBasic bookPerPage={bookPerPage} totalBooks={books.length} paginate = {paginate}/>
+     </div>
+       
       </div>
     </>
   );
